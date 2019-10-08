88057012d2052ae9c3b7060c37c63c99de41991e
thorchr <thorchrelind@gmail.com>
2019-10-08 15:16:33 +0200

    Implementing weather api

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,19 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+$(document).ready(function(){
+	getWeather();
+})
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+function getWeather(searchQuery){
+	var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&APPID="+apiKey;
+
+	$.ajax(url, {success: function(data){
+		$(".city").text(data.name);
+		$(".coord").text(data.coord.lon);
+		$(".temp").text(data.main.temp);
+		$(".wind").text(data.wind.speed);
+	}})
+}
+
+function searchWeather(){
+	var searchQuery = $(".search").val();
+	getWeather(searchQuery);
 }
